92b19463c80164656c5b5484014c6148af9541e9
alexhessami <104582633+alexhessami@users.noreply.github.com>
2024-02-08 11:53:46 -0800

    Update home.nix

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,1 +1,69 @@
+{
+  config,
+  pkgs,
+  ...
+}: {
+  home = {
+    homeDirectory = "/home/vlad";
+
+    packages = with pkgs; [
+      asciiquarium
+      bat
+      bottom
+      cachix
+      cmatrix
+      coursier
+      cowsay
+      delta
+      direnv
+      docker
+      docker-compose
+      doppler
+      duf
+      fd
+      figlet
+      fx
+      fzf
+      gh
+      git
+      glow
+      graalvm17-ce
+      graphviz
+      gum
+      heroku
+      htop
+      ipfetch
+      keychain
+      less
+      lolcat
+      mosh
+      ncdu
+      neofetch
+      neovim
+      nix-direnv
+      ookla-speedtest
+      pulumi
+      ripgrep
+      ripgrep-all
+      scala-cli
+      sl
+      stow
+      sublime4
+      tig
+      tree
+      wl-clipboard
+      zsh
+    ];
+
+    stateVersion = "22.11";
+    username = "vlad";
+  };
+
+  nixpkgs.config = {
+    allowUnfree = true;
+    allowUnfreePredicate = _: true;
+  };
+
+  programs.home-manager.enable = true;
+}
 
